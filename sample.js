53eb7a38253f931fe1f21837a9fcd06dbba8257e
10xdev4u-alt <10xdev4u@gmail.com>
2026-01-02 10:49:21 +0530

    Removed The Files

--- sample.js
+++
@@ -1,8 +0,0 @@
-// Sample JavaScript file for testing Mr.the-ai-dev
-console.log("Hello from Prince's code!");
-
-function addNumbers(a, b) {
-    return a + b;
-}
-
-console.log("Testing addition: " + addNumbers(5, 3));
